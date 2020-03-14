a2140d656bc6f56df94143c1feec16a848c1de34
S9379992 <s15579@pjwstk.edu.pl>
2020-03-14 18:02:22 +0100

    !
    first commit ot master

---
+++ views/home.php
@@ -0,0 +1,25 @@
+<div class="container mainContainer">
+
+    <div class="row">
+  <div class="col-md-8">
+        
+        <h2>Recent tweets</h2>
+        
+        <?php displayTweets('public'); ?>
+      
+        </div>
+  <div class="col-md-4">
+        
+        <?php displaySearch(); ?>
+      
+      <hr>
+      
+      <?php displayTweetBox(); ?>
+        
+        </div>
+</div>
+<!-- <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="1234.mp4" type="video/mp4">
+</video>
+     -->
+</div>
